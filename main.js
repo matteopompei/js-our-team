951b1fb9fa4d53d09cc87a3cdce7604132d5bd37
Matteo Pompei <datxizzan@gmail.com>
2022-01-10 16:09:25 +0100

    Bonus fatto

--- main.js
+++ main.js
@@ -49,3 +49,41 @@ for (key in team) {
   </div>
   `;
 }
+
+const formButton = document.getElementById("addMemberButton");
+let formName = document.getElementById("name");
+let formRole = document.getElementById("role");
+let formImage = document.getElementById("image");
+
+formButton.addEventListener("click", function(){
+  const newMember = {
+    name: formName.value,
+    role: formRole.value,
+    image: formImage.value,
+  }
+
+  console.log(newMember);
+
+  team.push(newMember);
+
+  teamContainer.innerHTML += `
+  <div class="team-card">
+    <div class="card-image">
+      <img
+      src="${newMember.image}"
+      alt="${newMember.name}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${newMember.name}</h3>
+      <p>${newMember.role}</p>
+    </div>
+  </div>
+  `;
+
+  formName.value = "";
+  formRole.value = "";
+  formImage.value = "";
+
+  console.log(team);
+});
